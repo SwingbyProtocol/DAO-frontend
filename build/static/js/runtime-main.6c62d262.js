898726ed4660ce732e23e7a906d9bce6f053f061
syrohei <syrohei@gmail.com>
2023-03-10 23:11:14 +0900

    feat: update

--- build/static/js/runtime-main.6c62d262.js
+++ build/static/js/runtime-main.6c62d262.js
@@ -69,7 +69,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "static/js/" + ({}[chunkId]||chunkId) + "." + {"3":"b34ce94d","4":"550aeaa2","5":"3952624e","6":"7f5d363a"}[chunkId] + ".chunk.js"
+/******/ 		return __webpack_require__.p + "static/js/" + ({}[chunkId]||chunkId) + "." + {"3":"b34ce94d","4":"550aeaa2","5":"38176aa8","6":"7f5d363a"}[chunkId] + ".chunk.js"
 /******/ 	}
 /******/
 /******/ 	// The require function
